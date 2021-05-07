a33ccadef3d9663085da46f3d9ee382664a5d3a8
Harshil Goti <harshilgoti2209@gmail.com>
2021-05-07 18:48:56 +0530

    deploy

--- Project Work/Backend/dynamictimetable/settings.py
+++ Project Work/Backend/dynamictimetable/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'y8xh4wb(ke^8=9mezbi4h&9g(^^c43_v)9uepjucj1ana%e%7o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
